f9e175cb879e09be63251a596952d9a31432ec0f
Auston <shuowen.chang@students.mq.edu.au>
2024-08-19 15:19:43 +1000

    week 4 updates

--- src/Actor.java
+++ src/Actor.java
@@ -1,12 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
 
 public abstract class Actor {
-    Cell loc;
-    Color col;
+  Color color;
+  Cell loc;
+  ArrayList<Polygon> polygons;
 
-    void paint(Graphics g){
-        g.setColor(col);
-        g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    }
+  public void paint(Graphics g) {
+    for (Polygon p : polygons){
+      g.setColor(color);
+    // g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+      g.fillPolygon(p);
+      g.setColor(Color.GRAY);
+    // g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+      g.drawPolygon(p);
+    }  
+  }
 }
--- src/Stage.java
+++ src/Stage.java
@@ -1,22 +1,36 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Stage {
-    Grid grid = new Grid();
-    Actor birdd;
-    Actor dogg;
-    Actor catt;
+  Grid grid;
+  // Actor cat;
+  // Actor dog;
+  // Actor bird;
+  ArrayList <Actor> actors = new ArrayList<>();
 
-    public Stage(){
-        birdd = new Bird(grid.cells[1][1]);
-        dogg = new Dog(grid.cells[18][6]);
-        catt = new Cat(grid.cells[10][17]);
-    }
+  public Stage() {
+    grid = new Grid();
+    actors.add(new Cat(grid.cellAtColRow(0, 0)));
+    actors.add(new Dog(grid.cellAtColRow(0, 15)));
+    actors.add(new Bird(grid.cellAtColRow(12, 9)));
+  
 
-    void paint(Graphics g, Point p){
-        grid.paint(g, p);
-        birdd.paint(g);
-        dogg.paint(g);
-        catt.paint(g);
+    // cat = new Cat(grid.cellAtColRow(0, 0));
+    // dog = new Dog(grid.cellAtColRow(0, 15));
+    // bird = new Bird(grid.cellAtColRow(12, 9));
+    
+  }
+
+  public void paint(Graphics g, Point mouseLoc) {
+    grid.paint(g, mouseLoc);
+    for (Actor a : actors){
+      a.paint(g);
     }
+    
+
+    // cat.paint(g);
+    // dog.paint(g);
+    // bird.paint(g);
+  }
 }
